b39030192b69b575ebfe0eb5ef6caeb898fa2bbd
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-11-12 13:39:28 +0200

    Stuff.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerInput : MonoBehaviour
+{
+
+    private CharacterController2D _characterController2D;
+
+	public void Start ()
+	{
+	    _characterController2D = GetComponent<CharacterController2D>();
+	}
+	
+	public void FixedUpdate () 
+    {
+	    if (Input.GetKey(KeyCode.LeftArrow))
+	    {
+	        _characterController2D.move(new Vector3(-0.4f, 0));
+	    }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _characterController2D.move(new Vector3(0, 0.8f));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _characterController2D.move(new Vector3(0.4f, 0));
+        }
+	}
+}
