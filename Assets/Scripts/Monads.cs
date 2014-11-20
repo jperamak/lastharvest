2157a330a78bf4a2ee2661abaecc65f10a7de724
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-11-20 12:13:25 +0200

    SoundEffect-script

---
+++ Assets/Scripts/Monads.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    public static class Monads
+    {
+        public static TSource Do<TSource>(this TSource obj, Action<TSource> action)
+            where TSource : class
+        {
+            if (obj != default(TSource))
+            {
+                action(obj);
+            }
+            return obj;
+        }
+    }
+}
---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SoundEffect : MonoBehaviour
+    {
+        public string name = "Some Effect";
+
+        [SerializeField]
+        private List<AudioSource> _audioClips = new List<AudioSource> {null}; //initial size 1
+        public IEnumerable<AudioSource> AudioClips { get { return _audioClips; } }
+
+        public float minPitch = 1f;
+        public float maxPitch = 1f;
+
+        public float minVolume = 1f;
+        public float maxVolume = 1f;
+
+        public ClipCyclingMode Mode = ClipCyclingMode.None;
+
+        private int _currentClip;
+
+
+        public void PlayEffect()
+        {
+            switch (Mode)
+            {
+                case ClipCyclingMode.None:
+                    PlayEffect(_audioClips.FirstOrDefault());
+                    break;
+                case ClipCyclingMode.InOrder:
+                    if (_currentClip >= _audioClips.Count)
+                        _currentClip = 0;
+                    PlayEffect(_audioClips.FirstOrDefault());
+                    _currentClip++;
+                    break;
+                case ClipCyclingMode.Random:
+                    PlayEffect(_audioClips[Random.Range(0, _audioClips.Count - 1)]);
+                    break;
+            }
+        }
+
+        private void PlayEffect(AudioSource effect)
+        {
+            effect.Do(c => c.volume = Random.Range(minVolume, maxVolume))
+                .Do(c => c.pitch = Random.Range(minPitch, maxPitch))
+                .Do(c => c.Play());
+        }
+    }
+
+    public enum ClipCyclingMode
+    {
+        None,
+        InOrder,
+        Random
+    }
+
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+}
