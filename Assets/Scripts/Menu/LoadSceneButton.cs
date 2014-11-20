55cd4cb8355178f271c75761b3825bdacff22e6e
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-11-20 15:12:15 +0200

    Added Button-scripts

---
+++ Assets/Scripts/Menu/LoadSceneButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Menu
+{
+    [RequireComponent(typeof(GUITexture))]
+    public class LoadSceneButton : ButtonBase
+    {
+        public string sceneName;
+
+        protected override void OnButtonClicked()
+        {
+            if (string.IsNullOrEmpty(sceneName))
+            {
+                Debug.LogError("Scene name not set");
+                return;
+            }
+
+            Application.LoadLevel(sceneName);
+        }
+    }
+}
