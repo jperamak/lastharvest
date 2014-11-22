a9f4553554e3c17383b7475c5ba26468bd979e65
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-11-22 17:46:33 +0200

    Aiming laser

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,9 +1,9 @@
 ﻿using Assets.Scripts;
 using UnityEngine;
-using System.Collections;
 using Assets.Scripts.Helpers;
 
 [RequireComponent(typeof(CharacterController2D))]
+[RequireComponent(typeof(LineRenderer))]
 public class PlayerInput : MonoBehaviour
 {
 	// movement config
@@ -33,12 +33,18 @@ public class PlayerInput : MonoBehaviour
     private Transform _grapplingPoint;
 
     private Camera _mainCamera;
+    private LineRenderer _lineRenderer;
 
 	public void Awake()
 	{
 		//_animator = GetComponent<Animator>();
 		_controller = GetComponent<CharacterController2D>();
 	    _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	    _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.SetVertexCount(2);
+        _lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        _lineRenderer.SetColors(Color.black, Color.red);
+        _lineRenderer.SetWidth(0.4f, 0.4f);
 		// listen to some events for illustration purposes
 		_controller.onControllerCollidedEvent += onControllerCollider;
 		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
@@ -163,6 +169,18 @@ public class PlayerInput : MonoBehaviour
 
 		_controller.move( _velocity * Time.deltaTime );
 	}
+
+    public void LateUpdate()
+    {
+        DrawHookAimLine(_mainCamera.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    private void DrawHookAimLine(Vector2 mousePosition)
+    {
+        var direction = (mousePosition - (Vector2)transform.position).normalized;
+        _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.SetPosition(1, transform.position + ((Vector3)direction * _hookPrefab.MaxLength));
+    }
 
     private void ThrowGrapplingHook(Vector3 direction)
     {
