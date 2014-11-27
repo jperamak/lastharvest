c67417108bd47bdc0d8ed9cee3cf75d7382f26a6
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-11-27 17:03:47 +0200

    Implemented LevelController for timer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,28 +8,23 @@ namespace Assets.Scripts
     public class GameController : MonoBehaviour
     {
         public int food;
-        private Player _player;
         public int currenLevel = 1;
         public List<FamilyMember> family;
-        private int harvestables;
 
         public void Start()
         {
-            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-            _player.Harvested += OnHarvested;
             DontDestroyOnLoad(this);
             StartFamily();
-            harvestables = GameObject.FindGameObjectsWithTag("Harvestable").Length;
         }
 
-        private void OnHarvested(object sender, HarvestEventArgs args)
+        public void GoToNextLevel()
         {
-            Debug.Log("Harvested: " + args.Harvestable);
-            Destroy(args.Harvestable.gameObject);
-            harvestables--;
-            if (harvestables <= 0)
-                Debug.Log("done");
-            //StartCoroutine(NextLevel ());
+            StartCoroutine(NextLevel());
+        }
+
+        public void FailLevel()
+        {
+            Debug.Log("Level failed :(");
         }
 
         private IEnumerator NextLevel()
@@ -40,16 +35,17 @@ namespace Assets.Scripts
             yield return new WaitForSeconds(5);
             //feed family
             Application.LoadLevel(++currenLevel);
-            harvestables = GameObject.FindGameObjectsWithTag("Harvestable").Length;
         }
 
         private void StartFamily()
         {
-            family = new List<FamilyMember>();
-            family.Add(new FamilyMember("Billy-Bob", 23));
-            family.Add(new FamilyMember("Wifey", 19));
-            family.Add(new FamilyMember("Babby1", 3));
-            family.Add(new FamilyMember("Babby2", 1));
+            family = new List<FamilyMember>
+            {
+                new FamilyMember("Billy-Bob", 23),
+                new FamilyMember("Wifey", 19),
+                new FamilyMember("Babby1", 3),
+                new FamilyMember("Babby2", 1)
+            };
         }
 
         private void Feed()
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class LevelController : MonoBehaviour
+    {
+        public float timeInSeconds = 180.0f;
+
+        private GameController _gameController;
+        private Player _player;
+
+        private readonly List<Harvestable> _harvestables = new List<Harvestable>(); 
+
+        public void Start()
+        {
+            _gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+            _harvestables.AddRange(GameObject.FindGameObjectsWithTag("Harvestable").Select(o => o.GetComponent<Harvestable>()));
+            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            _player.Harvested += OnHarvested;
+        }
+
+        public void Update()
+        {
+            timeInSeconds -= Time.deltaTime;
+            if (timeInSeconds < 0.0f)
+            {
+                _gameController.FailLevel();
+            }
+        }
+
+        private void OnHarvested(object sender, HarvestEventArgs args)
+        {
+            _harvestables.Remove(args.Harvestable);
+            Destroy(args.Harvestable.gameObject);
+            if (!_harvestables.Any())
+            {
+                _gameController.GoToNextLevel();
+            }
+        }
+    }
+}
