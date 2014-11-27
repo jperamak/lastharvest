ff707ece8fdbaae70703aa8e0bc943b7aad65c76
Jussi Perämäki <jussi.peramaki@gmail.com>
2014-11-27 11:29:26 +0200

    fixed IEnumerator problem in gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,64 +1,70 @@
-﻿using UnityEngine;
-using System.Linq;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 
-public class GameController : MonoBehaviour {
+namespace Assets.Scripts
+{
 
-	public static int food;
-	private Player _player;
-	public int currenLevel = 1;
-	public List<FamilyMember> family;
-	private int harvestables;
+    public class GameController : MonoBehaviour
+    {
 
-	void Start ()
-	{
-	    _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-	    _player.Harvested += OnHarvested;
-		DontDestroyOnLoad(this);
-		StartFamily ();
-		harvestables = GameObject.FindGameObjectsWithTag ("Harvestable").Length;
-	}
+        public static int food;
+        private Player _player;
+        public int currenLevel = 1;
+        public List<FamilyMember> family;
+        private int harvestables;
 
-	private void OnHarvested(object sender, HarvestEventArgs args)
-	{
-		Debug.Log ("Harvested: " + args.Harvestable);
-		Destroy (args.Harvestable.gameObject);
-		harvestables--;
-		if (harvestables <= 0)
-			Debug.Log ("done");
-			//StartCoroutine(NextLevel ());
-	}
+        void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            _player.Harvested += OnHarvested;
+            DontDestroyOnLoad(this);
+            StartFamily();
+            harvestables = GameObject.FindGameObjectsWithTag("Harvestable").Length;
+        }
 
-	//he type or namespace name `IEnumerator' could not be found, commented out to test animations/Tove
-//	IEnumerator NextLevel()
-//	{
-//		//loading screen
-//		Feed ();
-//		Application.LoadLevel ("LoadingScreen");
-//		yield return new WaitForSeconds (5);
-//		//feed family
-//		Application.LoadLevel (++currenLevel);
-//		harvestables = GameObject.FindGameObjectsWithTag ("Harvestable").Length;
-//	}
+        private void OnHarvested(object sender, HarvestEventArgs args)
+        {
+            Debug.Log("Harvested: " + args.Harvestable);
+            Destroy(args.Harvestable.gameObject);
+            harvestables--;
+            if (harvestables <= 0)
+                Debug.Log("done");
+            //StartCoroutine(NextLevel ());
+        }
 
-	private void StartFamily()
-	{	
-		family = new List<FamilyMember> ();
-		family.Add (new FamilyMember("Billy-Bob", 23));
-		family.Add (new FamilyMember("Wifey", 19));
-		family.Add (new FamilyMember("Babby1", 3));
-		family.Add (new FamilyMember("Babby2", 1));
-	}
+        //he type or namespace name `IEnumerator' could not be found, commented out to test animations/Tove
+        private IEnumerator NextLevel()
+        {
+            //loading screen
+            Feed();
+            Application.LoadLevel("LoadingScreen");
+            yield return new WaitForSeconds(5);
+            //feed family
+            Application.LoadLevel(++currenLevel);
+            harvestables = GameObject.FindGameObjectsWithTag("Harvestable").Length;
+        }
 
-	private void Feed()
-	{
-		foreach (FamilyMember fm in family.ToList())
-		{
-			food = fm.Feed(food);
-			if (!fm.DidSurvive())
-				family.Remove(fm);
-		}
-	}
+        private void StartFamily()
+        {
+            family = new List<FamilyMember>();
+            family.Add(new FamilyMember("Billy-Bob", 23));
+            family.Add(new FamilyMember("Wifey", 19));
+            family.Add(new FamilyMember("Babby1", 3));
+            family.Add(new FamilyMember("Babby2", 1));
+        }
 
+        private void Feed()
+        {
+            foreach (FamilyMember fm in family.ToList())
+            {
+                food = fm.Feed(food);
+                if (!fm.DidSurvive())
+                    family.Remove(fm);
+            }
+        }
+
+    }
 }
