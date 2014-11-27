8d46e3cafcafb3ba5049d1d5c161b1208dec74f0
Jussi Perämäki <jussi.peramaki@gmail.com>
2014-11-27 13:51:18 +0200

    implemented wind

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+using Assets.Scripts.Helpers;
+
+
+public class Wind : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 _windSpeed;
+    public Vector3 windSpeed 
+    {
+        get {return _windSpeed;}
+        set {_windSpeed = value;}
+    }
+    private float angle;
+    private Vector3 axis;
+
+    public void Start()
+    {
+        this.transform.localRotation.ToAngleAxis(out angle, out axis);    
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Tags.Player))
+            other.GetComponent<PlayerInput>().windSpeed = Quaternion.AngleAxis(angle, axis) * _windSpeed;
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Tags.Player))
+            other.GetComponent<PlayerInput>().windSpeed = Vector3.zero;
+    }
+}
