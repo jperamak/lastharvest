2e7c6fbe9f545c9f5deabab34e6a9f63363a7eb5
Vesa Paakkanen <vesa.paakkanen@rovio.com>
2014-11-28 12:47:31 +0200

    Improved Geysir

--- Assets/Scripts/Geyser.cs
+++ Assets/Scripts/Geyser.cs
@@ -6,10 +6,11 @@ namespace Assets.Scripts
     {
         public Vector2 size = new Vector2(2, 2);
         public float duration = 1.0f;
-        
-        public float intervalInSeconds = 5.0f;
-        private float _currentTime;
+	    public float speed = 5.0f;
 
+        public float intervalInSeconds = 5.0f;
+        
+		private float _currentTime;
         private bool _isSprouting;
         private float _sproutingTime;
         private float _timeToFullSize;
@@ -20,9 +21,13 @@ namespace Assets.Scripts
         {
             if (duration > intervalInSeconds)
             {
-                Debug.Log("Duration has to be shorter than interval");
+                Debug.LogError("Duration has to be shorter than interval");
             }
-            _timeToFullSize = duration/2.0f;
+	        if (speed < 2.0f)
+	        {
+		        Debug.Log("Speed has to be greater than 2");
+	        }
+            _timeToFullSize = duration/speed;
             _originalScale = transform.localScale;
         }
 
@@ -42,15 +47,15 @@ namespace Assets.Scripts
                     xSize = Mathf.Lerp(_originalScale.x, size.x, _sproutingTime / _timeToFullSize);
                     ySize = Mathf.Lerp(_originalScale.y, size.y, _sproutingTime / _timeToFullSize);
                     transform.position = transform.position + new Vector3((xSize - transform.localScale.x) / 2.0f, (ySize - transform.localScale.y) / 2.0f);
+					transform.localScale = new Vector3(xSize, ySize, 1);
                 }
-                else
+                else if (_sproutingTime > duration - _timeToFullSize)
                 {
-                    xSize = Mathf.Lerp(size.x, _originalScale.x, (_sproutingTime - _timeToFullSize) / _timeToFullSize);
-                    ySize = Mathf.Lerp(size.y, _originalScale.y, (_sproutingTime - _timeToFullSize) / _timeToFullSize);
+					xSize = Mathf.Lerp(size.x, _originalScale.x, (_sproutingTime - (duration - _timeToFullSize)) / _timeToFullSize);
+					ySize = Mathf.Lerp(size.y, _originalScale.y, (_sproutingTime - (duration - _timeToFullSize)) / _timeToFullSize);
                     transform.position = transform.position + new Vector3((xSize - transform.localScale.x) / 2.0f, (ySize - transform.localScale.y) / 2.0f);
+					transform.localScale = new Vector3(xSize, ySize, 1);
                 }
-
-                transform.localScale = new Vector3(xSize, ySize, 1);
                 
                 _sproutingTime += Time.deltaTime;
                 if (_sproutingTime >= duration)
